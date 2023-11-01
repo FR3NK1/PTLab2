17792ec8a7f9e70c9ad3f1546f3b263a443063d1
Dmitriy Mironov <mironov_2001@mail.ru>
2023-11-01 21:33:45 +0300

    Добавил уменьшение количества товара

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('buy/<int:product_id>/', views.PurchaseCreate.as_view(), name='buy'),
-    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),\
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('delete_from_cart/<int:product_id>/', views.delete_from_cart, name='delete_from_cart'),
     path('cart/', views.cart_detail, name='cart_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -15,7 +15,19 @@ def add_to_cart(request, product_id):
     product_id_str = str(product_id)
     cart[product_id_str] = cart.get(product_id_str, 0) + 1
     request.session['cart'] = cart
-    return redirect('index')
+    return redirect('/cart')
+
+def delete_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    product_id_str = str(product_id)
+    new_amount = cart.get(product_id_str, 1) - 1
+    if new_amount > 0:
+        cart[product_id_str] = new_amount
+    else:
+        del cart[product_id_str]
+    request.session['cart'] = cart
+    return redirect('/cart')
+    
 
 def cart_detail(request):
     cart = request.session.get('cart', {})
